dd2d3939e25cce9c9ba20a8d81657c5b4bdf2762
Julio Biason <julio.biason@gmail.com>
2018-10-18 10:01:57 -0300

    User election

--- lottery/admin.py
+++ lottery/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 
 from lottery.models import Person
 
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['pk']
+    ordering = ['pk']
+    actions = ['select_one']
+
+    def select_one(self, request, queryset):
+        selected = Person.objects.order_by('?').first()
+
+        # XXX check the origin database
+        self.message_user(request, '{} ({}), I CHOOSE YOU!!!'.format(
+                          selected.name,
+                          selected.email))
+
 # Register your models here.
-admin.site.register(Person)
+admin.site.register(Person, PersonAdmin)
