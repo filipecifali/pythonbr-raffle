d48ad50571f8caf3eb8e3fb4ef2090e4663c3168
Julio Biason <julio.biason@gmail.com>
2018-10-19 08:51:54 -0300

    Check registration first

--- lottery/models.py
+++ lottery/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
-# Create your models here.
 class Person(models.Model):
+    """A user registered for the raffle."""
     name = models.CharField(max_length=100)
     email = models.EmailField()
+
+
+class Registration(models.Model):
+    """Emails registered on the event."""
+    email = models.EmailField(primary_key=True)
